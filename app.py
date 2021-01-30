67c75f621605a8dd0f57d6545e2169869c69f8f4
Udit Kumar <uditkumarussa@gmail.com>
2021-01-30 21:17:05 +0530

    fixed font reducing error

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from hand import begin_writing_text
+import random
 with open("text/quotes.txt", 'r') as qoute:
     lines = qoute.readlines()
     for line in lines:
-
-        if line:
+        bg_img = ""
+        with open("text/random_bg.txt") as bg_file:
+            i_names = bg_file.readlines()
+            img_names = []
+            for name in i_names:
+                if name.strip():
+                    img_names.append(name)
+            i_names.clear()
+            # print(img_names)
+            bg_img = img_names[random.randint(0,len(img_names)-1)].strip()
+        print(bg_img,"bg")
+        if line.strip():
             args = {
                 "text": line.split(" ~ ")[0],
                 "author": line.split(" ~ ")[1],
-                "image": "background/bg2.jpg",
-                "color": "255 255 255",
+                "image": "background/"+bg_img.split(" ~ ")[0],
+                "color": bg_img.split(" ~ ")[1],
                 "font": "fonts/LeagueSpartan-Bold.otf",
                 "fontSize":50,
                 "res": 60
--- hand.py
+++ hand.py
@@ -25,7 +25,7 @@ def draw_multiple_line_text(image, imgName, text, author, fontName, fontSize, fo
         W, H = image.size
         # y_text = (H-line_height*(len(lines)+6))//2
         y_text = initial_height
-        print(lines, y_text, textwrapWidth)
+        # print(lines, y_text, textwrapWidth)
         max_line_width = 0
         for line in lines:
             line_width, line_height = font.getsize(line)
@@ -35,15 +35,26 @@ def draw_multiple_line_text(image, imgName, text, author, fontName, fontSize, fo
             y_text += line_height
             max_line_width = max(max_line_width,line_width)
             if y_text+line_height > 600:
-                print("This text won't lie in this page",fontSize, textwrapWidth, initial_height)
+                # print("This text won't lie in this page",fontSize, textwrapWidth, initial_height)
                 # image.show()
                 # time.sleep(5)
                 if initial_height > 100:
-                    author_logo_gap-=1
-                    write(text, author, imgName, text_color, fontName, fontSize, res, num, textwrapWidth, initial_height-10)
+                    if author_logo_gap >=130:
+                        print("Pushing text downward")
+                        print()
+                        author_logo_gap-=1
+                        print("Pushing text upward")
+                        print()
+                        write(text, author, imgName, text_color, fontName, fontSize, res, num, textwrapWidth, initial_height-10)
+                    elif author_logo_gap > 150:
+                        write(text, author, imgName, text_color, fontName, fontSize, res, num, textwrapWidth, initial_height-10)
                 else:
+                    print("Reducing font size")
+                    print()
                     wrap_width = textwrapWidth
                     if W-max_line_width >= 200:
+                        print("Increasing text warp width")
+                        print()
                         wrap_width+=3
                     write(text, author, imgName, text_color, fontName, fontSize-1, res, num, wrap_width, initial_height)
                 break
@@ -53,7 +64,7 @@ def draw_multiple_line_text(image, imgName, text, author, fontName, fontSize, fo
         font = ImageFont.truetype("fonts/efont.otf", fontSize)
         line_width, line_height = font.getsize(line)
         w, h = draw.textsize(line, font=font)
-        print(H-160,"edunil")
+        # print(H-160,"edunil")
         draw.text(((W - w) / 2, H-160),
                 line, font=font, fill=text_color)
         # placing logo end
@@ -62,7 +73,7 @@ def draw_multiple_line_text(image, imgName, text, author, fontName, fontSize, fo
         font = ImageFont.truetype("fonts/author.otf", fontSize)
         line = author
         line_width, line_height = font.getsize(line)
-        print(y_text+line_height,"author")
+        # print(y_text+line_height,"author")
         w, h = draw.textsize(line, font=font)
         draw.text(((W - w) / 2, y_text+line_height),
                 line, font=font, fill=text_color)
@@ -71,22 +82,33 @@ def draw_multiple_line_text(image, imgName, text, author, fontName, fontSize, fo
         
         if (H-160)-(y_text+line_height)>=author_logo_gap:
             print("saving image")
+            print()
             save_img(num, image, res)
         else:
-            print("less gap",fontSize, textwrapWidth, initial_height)
+            # print("less gap",fontSize, textwrapWidth, initial_height)
 
             # image.show()
             # time.sleep(5)
             if initial_height >100:
-                author_logo_gap-=1
-                write(text, author, imgName, text_color, fontName, fontSize, res, num, textwrapWidth, initial_height-10)
+                if author_logo_gap >=130:
+                    print("Pushing text downward")
+                    print()
+                    author_logo_gap-=1
+                    print("Pushing text upward")
+                    print()
+                    write(text, author, imgName, text_color, fontName, fontSize, res, num, textwrapWidth, initial_height-10)
+                elif author_logo_gap > 150:
+                    write(text, author, imgName, text_color, fontName, fontSize, res, num, textwrapWidth, initial_height-10)
             else:
+                print("Reducing font size")
+                print()
                 wrap_width = textwrapWidth
                 if W-max_line_width >= 200:
                     wrap_width+=3
                 write(text, author, imgName, text_color, fontName, fontSize-1, res, num, wrap_width, initial_height)
     else:
-        print(done_status)
+        # print(done_status)
+        pass
 
 
 total_pages = 0
